fa96b103b75e63d7df6ec74519abda014185cad1
Habibullah Sirat <habibullah.sirat.001@gmail.com>
2025-06-16 19:39:05 +0600

    Create two new two classes.

--- script.js
+++ script.js
@@ -10,6 +10,51 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+class Workout {
+    date = new Date();
+    id = (Date.now() + '').slice(-10);
+
+    constructor (coords, distance, duration) {
+        this.coords = coords; // [lat, lng]
+        this.distance = distance; // in km
+        this.duration = duration; // in min
+    }
+}
+
+class Running extends Workout {
+    constructor(coords, distance, duration, cadence) {
+        super(coords, distance, duration);
+        this.cadence = cadence;
+        this.calcPace();
+    }
+
+    calcPace() {
+        //km
+        this.pace = this.duration / this.distance;
+        return this.pace;
+    }
+};
+
+class Cycling extends Workout {
+    constructor(coords, distance, duration, elevationGain) {
+        super(coords, distance, duration);
+        this.elevationGain = elevationGain;
+        this.calcSpeed();
+    }
+
+    calcSpeed() {
+        // in km/h
+        this.spped = this.distance / (this.duration / 60);
+        return this.calcSpeed;
+    }
+};
+
+const run1 = new Running([39, -12], 5.2, 24, 172);
+const cyl1 = new Cycling([39, -12], 5.2, 27, 523);
+console.log(run1, cyl1);
+
+///////////////////////////////////////////////////////////
+// Main Application
 class App {
     #map;
     #mapEvent;
